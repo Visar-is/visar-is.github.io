33e022254f9779e1e0f6c2a5c333f3e2aadac5e8
Brian Suda <brian@suda.co.uk>
2013-10-23 16:17:24 +0000

    switching to python

--- audio/play.py
+++ audio/play.py
@@ -1,3 +1,33 @@
+# This is our application object. It could have any name,
+# except when using mod_wsgi where it must be "application"
+def application( # It accepts two arguments:
+      # environ points to a dictionary containing CGI like environment variables
+      # which is filled by the server for each received request from the client
+      environ,
+      # start_response is a callback function supplied by the server
+      # which will be used to send the HTTP status and headers to the server
+      start_response):
+
+   # build the response body possibly using the environ dictionary
+   response_body = 'The request method was %s' % environ['REQUEST_METHOD']
+
+   # HTTP response code and message
+   status = '200 OK'
+
+   # These are HTTP headers expected by the client.
+   # They must be wrapped as a list of tupled pairs:
+   # [(Header name, Header value)].
+   response_headers = [('Content-Type', 'text/plain'),
+                       ('Content-Length', str(len(response_body)))]
+
+   # Send them to the server using the supplied function
+   start_response(status, response_headers)
+
+   # Return the response body.
+   # Notice it is wrapped in a list although it could be any iterable.
+   return [response_body]
+
+'''
 <?php
 // this file does two simple things.
 // 1. Returns cached WAV files
@@ -10,7 +40,6 @@ if(!isset($_GET['lang']) || !isset($_GET['text'])) { return; }
 $text = substr($_GET['text'],0,2000);
 $lang = substr($_GET['lang'],0,2);
 $salt = 'ty79hgue12t78r6219832528!T76852&%%2092';
-
 
 
 // create a cache key based on the text and lang
@@ -100,3 +129,4 @@ function synthesize($text, $text_type='text/plain') {
 }
 
 ?>
+'''
