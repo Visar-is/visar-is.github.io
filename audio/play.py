a66fa0b26df4723d4bf0a15e88ff308ad6f54126
Brian Suda <brian@suda.co.uk>
2013-10-25 09:26:48 +0000

    Added all the famfamfam flag icons

--- audio/play.py
+++ audio/play.py
@@ -1,48 +1,52 @@
 #!/usr/bin/env python
-
-from wsgiref.simple_server import make_server
-from cgi import parse_qs, escape
-import hashlib
 import json
+import urllib.request
+from urllib.error import HTTPError
 import sys
 import getopt
-from urllib2 import Request, urlopen
-from urllib import urlencode
-import httplib
+import http.client, urllib.parse
+#from hashlib import md5,sha1
+import hashlib
 
 def application(environ, start_response):
-
+   pwd = '/home/briansuda/webapps/visar_static/htdocs/static/audio'
    # Returns a dictionary containing lists as values.
 
-   d = parse_qs(environ['QUERY_STRING'])
+   d = urllib.parse.parse_qs(environ['QUERY_STRING'])
    text = d.get('text', 'Something went wrong')[0]
    lang = d.get('lang', 'is')[0]
-   salt = 'hy89ry7dbu3e8932hio!/(9!!78!gy8!790)'
-   
+   salt = 'hy89ry7dbu3e8932hio'
+
    md5 = hashlib.md5()
-   md5.update(text+lang+salt)
-   out = md5.hexdigest()
+   out = text+lang+salt
+   out = out.encode('utf-8')
+   md5.update(out)
+   out = hashlib.sha1(out).hexdigest()
 
    # see if there is already a cache of this file
    try:
-      with open('cache/%s.mp3'%out) as f:
+      with open('%s/.cache/%s.mp3'%(pwd,out),'r') as f:
          response_body = f.read()
    # if not, go make one!
    except:
-   
       c = Client("brian@suda.co.uk", "i4lMC3tJ5KVlAPwKQHVnwk0XSF3yOgQt")
       c.Start()
-      
-      #get URL to the generated sound file
+
+     #get URL to the generated sound file
       soundUrl = c.CreateSpeechFile(text, "text/plain", "is_dora", "mp3/22050")
 
-      response = urlopen(soundUrl)
+      response = urllib.request.urlopen(soundUrl)
       response_body = response.read()
+      #response_body = out
 
       # save this to a file to speed it up next time!
-      myFile = open('cache/%s.mp3'%out, 'wb')
-      myFile.write(response_body)
-      myFile.close()
+      try:
+         myFile = open('%s/.cache/%s.mp3'%(pwd,out), 'wb')
+         myFile.write(response_body)
+         myFile.close()
+      except Exception:
+         response_body = "Unexpected error: %s"%sys.exc_info()[0]
+
 
    status = '200 OK'
 
@@ -52,6 +56,8 @@ def application(environ, start_response):
    start_response(status, response_headers)
 
    return [response_body]
+
+
 
 class Client(object):
     __web_comm = None
@@ -70,8 +76,6 @@ class Client(object):
                 "params": additional}
         content = self.__web_comm.CallMethod("/api/saas/rest/speechfiles/", params)
         content = json.loads( content )
-        print content
-
         return content["soundUrl"]
 
 class Communicator(object):
@@ -86,7 +90,7 @@ class Communicator(object):
         md5 = hashlib.md5()
         md5.update(password.encode())
         self.__password = md5.hexdigest()
-        self.__gettoken_params = urlencode({'email': email})
+        self.__gettoken_params = urllib.parse.urlencode({'email': email})
 
     def __del__(self):
         if self.__rest != None:
@@ -94,35 +98,32 @@ class Communicator(object):
 
     def Start(self):
         try:
-            self.__rest = httplib.HTTPConnection('api.ivona.com', 80) 
-        except http.client.HTTPException as ex: 
+            self.__rest = http.client.HTTPConnection('api.ivona.com', 80)
+        except http.client.HTTPException as ex:
             print(ex, "\n")
 
     def CallMethod(self, method_url, params={}, method="POST"):
-        #get token     
+        #get token
         self.__rest.request("POST", "/api/saas/rest/tokens/", self.__gettoken_params, self.__headers)
         token = eval(self.__rest.getresponse().read().decode())
-
+ 
         #compute md5
         md5 = hashlib.md5()
         md5.update(self.__password.encode())
-        md5.update(token.encode())    
+        md5.update(token.encode())
         tokenized_pass = md5.hexdigest()
-
+ 
         params["token"] = token
         params["md5"] = tokenized_pass
-
+ 
         #call mathod
         if method == "GET" or method == "PUT" or method == "DELETE":
-            method_url += "?" + urlencode(params)
+            method_url += "?" + urllib.parse.urlencode(params)
             self.__rest.request(method, method_url)
         else:
-            self.__rest.request(method, method_url, urlencode(params), self.__headers)
-
+            self.__rest.request(method, method_url, urllib.parse.urlencode(params), self.__headers)
+ 
         return self.__rest.getresponse().read().decode()
 
-#httpd = make_server('localhost', 8051, application)
-# Now it is serve_forever() in instead of handle_request().
-# In Windows you can kill it in the Task Manager (python.exe).
-# In Linux a Ctrl-C will do it.
-#httpd.serve_forever()
+
+
