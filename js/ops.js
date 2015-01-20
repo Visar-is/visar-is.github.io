46b44de90de66414fe29ddb769c6111926553204
Barnaby Walters <barnaby@waterpigs.co.uk>
2015-01-20 13:02:29 +0000

    Added styles, javascript for collapsible sections in Konnun ops

--- js/ops.js
+++ js/ops.js
@@ -23,8 +23,62 @@ $(document).ready(function() {
 		});
 	});
 	
-	// Solves Chrome Bug #350893
-	// Causes a flicker, should only do this for Chrome browsers!
-	$("object").each(function(){ $(this).load($(this).attr("data")); })
+	// Solves Chrome Bug #350893.
+	$("object").each(function() { $(this).load(this.getAttribute("data")); });
 	
+	(function () {
+		$('.collapsible-header').click(function (event) {
+			var collectionEl = $(event.target).closest('.collapsible');
+			var ddimg = collectionEl.find('.collapsible-expanded-indicator img');
+			var currentSrc = ddimg.prop('src');
+			event.preventDefault();
+
+			if (collectionEl.hasClass('collapsed')) {
+				collectionEl.removeClass('collapsed');
+			} else {
+				collectionEl.addClass('collapsed');
+			}
+			ddimg.prop('src', ddimg.attr('data-toggle-src'));
+			ddimg.attr('data-toggle-src', currentSrc);
+		});
+	}());
+	$('.collapsible.collapse-on-load:not(.collapsed) .collapsible-header').click();
+
+	// Apply default sort.
+	window.setTimeout(function () {
+		$('.default-sort').each(function () {
+			sorttable.innerSortFunction.apply(this, []);
+		});
+	}, 100);
+	
+	
+	// Progressively enhance participant state forms.
+	$('.participant-state-form [type=submit]').remove();
+	$('.participant-state-form [name=call_status]').change(function () {
+		var callStatusEl = $(this);
+		var callStatusForm = callStatusEl.closest('form');
+		var data = callStatusForm.serialize();
+		console.log(data);
+
+		callStatusEl.prop('disabled', true);
+		// This call not working due to jQuery apparently changing the contents of .serialize()
+		$.ajax({
+			url: callStatusForm.attr('action'),
+			type: callStatusForm.attr('method'),
+			data: data,
+			processData: false,
+
+			success: function () {
+				callStatusEl.attr('data-saved-value', callStatusEl.val());
+			},
+
+			error: function () {
+				callStatusEl.val(callStatusEl.attr('data-saved-value'));
+			},
+
+			complete: function () {
+				callStatusEl.prop('disabled', false);
+			}
+		});
+	});
 });
