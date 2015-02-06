ed71e44068fdad5d59cdece225b3d726d643cf1c
Barnaby Walters <barnaby@waterpigs.co.uk>
2015-02-06 13:06:43 +0000

    Added old version of konnun for use with students survey

---
+++ js/old-students-konnun-min.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	// Determine if Header is Fixed
+	// Return corresponding height value
+	var header_padding = function() {
+		if ($('header').css('position') === 'fixed') {
+			var padding = $('header').height() + 45;
+		}
+		else {
+			var padding = 0;
+		}
+		return padding;
+	};
+
+	// Set Padding Top for #content
+	$('#content').css('padding-top', header_padding());
+
+	// Hide Message
+	$('.message-close').on('click', function() {
+		$(this).parent().fadeOut(function() {
+			// Update Padding Top for #content
+			$('#content').css('padding-top', header_padding());
+		});
+	});
+
+});
