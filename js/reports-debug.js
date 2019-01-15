deb3d28c383a311c7c1a2cd46a086146ed52a697
Barnaby Walters <barnaby@waterpigs.co.uk>
2019-01-15 15:38:31 +0100

    Added cohort-grade special case

--- js/reports-debug.js
+++ js/reports-debug.js
@@ -11198,11 +11198,27 @@ $(document).ready(function() {
 	// Longitudinal chart hover effects.
 	(function () {
 		var yslines = $('.longitudinal-lines line');
+		var processedClasses = [];
 
 		yslines.each(function(i, ysline) {
-			var schoolClass = ysline.getAttribute('class').split(' ').filter(function (c) { return c[0] == 's'; })[0];
-			var points = $('.point.' + schoolClass);
-			var lines = $('.longitudinal-lines line.' + schoolClass);
+			// We need a classname which identifies lines of the same class as the one we’re processing right now.
+			// Currently, this is not easily achievable across differen types of longitudinal chart, and fixing that
+			// would require regenerating many charts. Therefore, for the moment, I opted for two special cases: one
+			// handling longitudinal scatter charts (the original) and the other handling cohort charts.
+			var lineClass = ysline.getAttribute('class').split(' ').filter(function (c) {
+				// Is the currentl classname one which we can use to identify associated line and li.point elements?
+				// i.e. of the form `s\d+` for longitudinal schools or `cohort-grade-\d+` for grade cohort charts.
+				return c[0] == 's' || c.indexOf('cohort-grade-') != -1;
+			})[0];
+			
+			if (processedClasses.indexOf(lineClass) != -1) {
+				return; // Don’t double-process the same classname.
+			} else {
+				processedClasses.push(lineClass);
+			}
+			
+			var points = $('.point.' + lineClass);
+			var lines = $('.longitudinal-lines line.' + lineClass);
 			$('.point.' + schoolClass).last().addClass('last');
 
 			$(ysline).on('mouseover', function (event) {
