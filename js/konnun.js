8eb08bba75270d0f5b7e185ded9e722fbdacba23
Brian Suda <brian@suda.co.uk>
2019-03-29 22:38:27 +0000

    Fixing some weird IE11 bug regarding images in input labels

--- js/konnun.js
+++ js/konnun.js
@@ -66,7 +66,7 @@ $('.response-input').click(function (event) {
 		// The participant has clicked an unselected item.
 		if (inputEl.prop('type') == 'radio') {
 			questionEl.find('.response.selected').removeClass('selected');
-			inputEl.prop('checked', true);
+			inputEl.prop('checked', 'checked');
 		}
 		responseEl.addClass('selected');
 		
