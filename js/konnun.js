998bb2bbf55aac4d9defcb54dbdd07c3fea76dab
Brian Suda <brian@suda.co.uk>
2019-04-10 15:45:18 +0000

    Updating checkboxes

--- js/konnun.js
+++ js/konnun.js
@@ -66,6 +66,7 @@ $('.response-input').click(function (event) {
 		// The participant has clicked an unselected item.
 		if (inputEl.prop('type') == 'radio') {
 			questionEl.find('.response.selected').removeClass('selected');
+			inputEl.attr('checked', 'checked');
 			inputEl.prop('checked', true);
 		}
 		responseEl.addClass('selected');
