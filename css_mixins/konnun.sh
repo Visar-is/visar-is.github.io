140011d5482132e423e7b829bfb38671e33e87fe
Brian Suda <brian@suda.co.uk>
2025-07-14 17:58:37 +0100

    Cleaning-up the css structure

--- css_mixins/konnun.sh
+++ css_mixins/konnun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of CSS files
-CSS_FILES="app/variables.css base/base.css base/typography.css base/buttons.css base/forms.css konnun/web.css konnun/tablet.css konnun/mobile.css themes/default.css"
+CSS_FILES="app/variables.css base/base.css base/typography.css base/buttons.css base/forms.css konnun/web.css konnun/tablet.css konnun/mobile.css app/themes.css"
 
 # Output file
 OUTPUT="../css/konnun.css"
