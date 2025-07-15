8c9aef5273b3247f4fc867c11868befa8b07af0a
Brian Suda <brian@suda.co.uk>
2025-07-15 16:45:26 +0000

    Updating link styles

--- css_mixins/konnun.sh
+++ css_mixins/konnun.sh
@@ -8,7 +8,7 @@ css_files=(
 	"css_mixins/base/buttons.css"
 	"css_mixins/base/forms.css"
 	"css_mixins/base/links.css"
-	"css_mixins/base/messages.css"
+	"css_mixins/app/messages.css"
 	"css_mixins/konnun/web.css"
 	"css_mixins/konnun/tablet.css"
 	"css_mixins/konnun/mobile.css"
