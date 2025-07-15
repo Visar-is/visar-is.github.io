9df6482a5998ecbd4c81705092469005fda9236d
Brian Suda <brian@suda.co.uk>
2025-07-15 11:22:36 +0000

    removing node folder

--- css_mixins/konnun.sh
+++ css_mixins/konnun.sh
@@ -4,9 +4,11 @@
 CSS_FILES="app/variables.css base/base.css base/typography.css base/buttons.css base/forms.css konnun/web.css konnun/tablet.css konnun/mobile.css app/themes.css"
 
 # Output file
-OUTPUT="../css/konnun.css"
+OUTPUT="../css/konnun"
 
 # Combine
-cat $CSS_FILES > $OUTPUT
+cat $CSS_FILES > "${OUTPUT}.css"
 
-echo "✅ CSS files combined into $OUTPUT"
+echo "✅ CSS files combined into ${OUTPUT}.css"
+
+uglifycss "${OUTPUT}.css" > "${OUTPUT}.min.css"
--- css_mixins/ops.sh
+++ css_mixins/ops.sh
@@ -4,9 +4,11 @@
 CSS_FILES="app/variables.css app/icons.css base/base.css base/links.css base/typography.css base/navigation.css base/lists.css base/buttons.css base/forms.css base/skeleton.css app/messages.css ops/web.css app/themes.css ../js/lib/jsdifflib/diffview.css"
 
 # Output file
-OUTPUT="../css/ops.css"
+OUTPUT="../css/ops"
 
 # Combine
-cat $CSS_FILES > $OUTPUT
+cat $CSS_FILES > "${OUTPUT}.css"
 
-echo "✅ CSS files combined into $OUTPUT"
+echo "✅ CSS files combined into ${OUTPUT}.css"
+
+uglifycss "${OUTPUT}.css" > "${OUTPUT}.min.css"
