ed87d3dc00477187486b35018e7cff6ccbee822c
Brian Suda <brian@suda.co.uk>
2025-07-28 13:30:58 +0000

    Updating JS fadeIn

--- js/reports-debug.js
+++ js/reports-debug.js
@@ -11247,7 +11247,7 @@ $(document).ready(function() {
 
 					var hoverBoxEl = chartEl.find('.hover-box[data-index='+pointEl.getAttribute('data-index')+']');
 					hoverBoxEl.text(pointEl.getAttribute('data-name'));
-					hoverBoxEl.fadeIn( 5000, function() {
+					hoverBoxEl.fadeIn( 500, function() {
     					// Animation complete
 					});
 					hoverBoxEl.css('top', $(pointEl).position().top + 20);
@@ -11433,7 +11433,9 @@ $(document).ready(function() {
 
 						var hoverBoxEl = chartEl.find('.hover-box[data-index='+pointEl.getAttribute('data-index')+']');
 						hoverBoxEl.text(pointEl.getAttribute('data-name'));
-						hoverBoxEl.show();
+						hoverBoxEl.fadeIn(500, function() { 
+							// animation ended
+						});
 						hoverBoxEl.css('top', $(pointEl).position().top + 20);
 						hoverBoxEl.css('left', $(pointEl).position().left - (hoverBoxEl.outerWidth() / 2))
 					})
