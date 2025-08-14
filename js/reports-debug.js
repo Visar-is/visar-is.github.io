3f3c16816b705ad6e5d1fa26a5bfbae9454178fa
Brian Suda <brian@suda.co.uk>
2025-08-14 13:48:05 +0000

    Adding bar hover

--- js/reports-debug.js
+++ js/reports-debug.js
@@ -11500,18 +11500,25 @@ $(document).ready(function() {
 	(function () {
 		$('.bar-chart').each(function (i, el) {
 			var chartEl = $(el);
+			console.log("Bar Chart Found!")
 
-			chartEl.find('li.bar').mouseover(function (event) {
+			chartEl.find('.bars .bar').mouseover(function (event) {
 				console.log(event.target)
 				// For the moment we can assume that bar chart legend items have a single class, vastly simplifying the hover code.
-				var hoverclass = event.target.getAttribute('class');
+				var hoverclass = 'compare'
+				if (event.target.getAttribute('class').includes('you')){
+					hoverClass = 'you'
+				}
 				chartEl.addClass('hovered');
 				chartEl.find('.' + hoverclass).addClass('hovered');
 			});
 
-			chartEl.find('li.bar').mouseout(function (event) {
+			chartEl.find('.bars .bar').mouseout(function (event) {
 				// For the moment we can assume that bar chart legend items have a single class, vastly simplifying the hover code.
-				var hoverclass = (event.target.getAttribute('class') || '');
+				var hoverclass = 'compare'
+				if (event.target.getAttribute('class').includes('you') || ''){
+					hoverClass = 'you'
+				}
 
 				chartEl.removeClass('hovered');
 				chartEl.find('.' + hoverclass).removeClass('hovered');
